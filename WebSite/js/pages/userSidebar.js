6ca2ba6f7d3b7bedcd540a0342e9f97377a9b221
Laptop-PC\Laptop <alexanddercrb@gmail.com>
2017-06-21 21:17:40 +0300

    added admin sidebar

---
+++ WebSite/js/pages/userSidebar.js
@@ -0,0 +1,9 @@
+﻿
+$(document).ready(function () {
+    var page = $('body').attr("id");
+    var element = $('.sidebar-offcanvas .' + page);
+    element.addClass('selectedMenu');
+    if (element.parent().parent().hasClass('collapse'))
+        element.parent().parent().addClass('in');
+
+})
